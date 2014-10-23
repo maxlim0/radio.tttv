c8196c98e649edaf1b332569adb0dec24ebb6c93
Max Solyaris <maxsolyaris@gmail.com>
2014-10-23 15:20:16 +0300

    delete t

--- t/autchk.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-	$us_role = $_SESSION['user_role_chat'];
-		if($us_role!= 1){
-			header("location: /");
-			exit();
-		}
-?>	
--- t/js/flash_chat.js
+++
@@ -1,1 +0,0 @@
-var pageHost = ((document.location.protocol == "https:") ? "https://" : "http://"); document.write("<a href='http://www.adobe.com/go/getflashplayer'><img src='"                                 + pageHost + "www.adobe.com/images/shared/download_buttons/get_flash_player.gif' alt='Get Adobe Flash player' /></a>" ); 
